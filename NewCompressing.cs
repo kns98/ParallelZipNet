49b732773db5046fb848d00124e81427dcc69778
Roman Ageev <roman.ageyev@devexpress.com>
2018-02-19 13:45:29 +0300

    Start working on PLinq-like approach

--- NewCompressing.cs
+++ NewCompressing.cs
@@ -53,13 +53,51 @@ namespace ParallelZipNet {
             dest.WriteInt32(chunkCount);
 
 
-            var chunks = ReadDecompressed(source)
-                .AsSingle()
+            // var chunks = ReadDecompressed(source)
+            //     .AsSingle()
+            //     .Select(CompressChunk)
+            //     .AsMultiple(4);
+
+            var chunks = ReadSource(source)
+                .AsParallel(4)
                 .Select(CompressChunk)
-                .AsMultiple(4);
+                .Select(x => {
+                    NewCompressing.Log("Compressed", x);
+                    return x;
+                })
+                .AsEnumerable();
 
             foreach(var chunk in chunks)
                 WriteCompressed(chunk, dest);            
+        }
+
+        public static IParallelContext<Chunk> AsParallel(this IEnumerable<Chunk> source, int jobNumber) {
+            return new ParallelContext<Chunk>(jobNumber, source);
+        }
+
+        public static IEnumerable<Chunk> ReadSource(StreamWrapper source) {
+            bool isLastChunk;
+            int chunkIndex = 0;
+            do {                
+                long bytesToRead = source.BytesToRead;
+                // if(bytesToRead == 0)
+                //     yield break;
+                isLastChunk = bytesToRead < Constants.CHUNK_SIZE;
+                int readBytes;
+                if(isLastChunk) 
+                    readBytes = (int)bytesToRead;
+                else
+                    readBytes = Constants.CHUNK_SIZE;
+                byte[] data = source.ReadBuffer(readBytes);
+                Chunk chunk = new Chunk(chunkIndex++, data);
+
+                NewCompressing.Log("Read", chunk);
+
+                yield return chunk;
+
+            }
+            while(!isLastChunk);            
+            
         }
     }
 
@@ -206,4 +244,97 @@ namespace ParallelZipNet {
             }
         }        
     }
+}
+
+public interface IParallelContext<T> {
+    IParallelContext<T> Select(Func<T, T> transform);
+    IEnumerable<T> AsEnumerable();
+}
+
+public class ParallelContext<T> : IParallelContext<T> where T : class {
+    readonly Job2<T>[] jobs;
+    readonly List<Func<T, T>> actions = new List<Func<T, T>>();
+
+    public ParallelContext(int jobNumber, IEnumerable<T> source) {
+        jobs = new Job2<T>[jobNumber];
+        for(int i = 0; i < jobs.Length; i++)
+            jobs[i] = new Job2<T>($"thread {i}", actions, source);
+    }
+
+    IParallelContext<T> IParallelContext<T>.Select(Func<T, T> action) {
+        actions.Add(action);
+        return this;
+    }    
+    IEnumerable<T> IParallelContext<T>.AsEnumerable() {
+        foreach(var job in jobs)
+            job.Start();
+
+        while(true) {
+            var jobsAlive = jobs.Where(job => job.IsAlive).ToArray();
+
+            if(jobsAlive.Length == 0)
+                yield break;
+
+            foreach(var job in jobsAlive) {
+                T result = job.Result;
+                if(result != null)
+                    yield return result;
+                else
+                    Thread.Yield();
+            }
+        }
+    }
+}
+
+public class Job2<T> where T : class {
+    readonly string name;
+    readonly IEnumerable<Func<T, T>> actions;
+    readonly Queue<T> accumulator = new Queue<T>();
+    readonly Thread thread;
+    readonly IEnumerable<T> source;
+
+    public bool IsAlive { 
+        get {
+            lock(accumulator) {
+                return accumulator.Count > 0 || thread.IsAlive;
+            }
+        }
+    }
+    public T Result {
+        get {
+            lock(accumulator) {
+                return accumulator.Count > 0 ? accumulator.Dequeue() : null;
+            }
+        }
+    }
+
+    public Job2(string name, IEnumerable<Func<T, T>> actions, IEnumerable<T> source) {
+        this.name = name;
+        this.actions = actions;
+        this.source = source;
+        thread = new Thread(Run) {
+            Name = name,
+            IsBackground = false
+        };
+
+    }
+    public void Start() {
+        thread.Start();
+    }
+
+    void Run() {
+        IEnumerable<T> results = source;
+        foreach(var action in actions)  
+            results = results.Select(action);
+        
+        // Note:
+        // It is wrong to read a source in multiple threads.
+        // I need to either use a lock or introduce a dedicated thread for such reading.
+        
+        foreach(T result in results) {
+            lock(accumulator) {
+                accumulator.Enqueue(result);
+            }
+        }
+    }        
 }
