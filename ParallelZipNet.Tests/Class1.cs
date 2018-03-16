3f080c8dc30ba7f4e5f36e2791843420cee4cbcd
Roman Ageev <roman.ageyev@devexpress.com>
2018-03-16 09:55:11 +0300

    Setup xUnit, vscoderunner and write the first test

--- ParallelZipNet.Tests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ParallelZipNet.Tests
-{
-    public class Class1
-    {
-    }
-}
---
+++ ParallelZipNet.Tests/FirstFixture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ParallelZipNet.Commands;
+using Xunit;
+
+namespace ParallelZipNet.Tests {
+    public class FirstFixture {
+        [Fact]
+        public void FirstTest() {
+            var section = new Section("test", new string[] { "test" }, new string[0]);
+            Assert.False(section.TryParse(new string[0], out Option option));
+        }
+    }
+}
