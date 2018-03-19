f765d4d4cf99e6326c83fad9d47523b5d2d1da10
Roman Ageev <roman.ageyev@devexpress.com>
2018-03-19 08:35:56 +0300

    Add packages - FakeItEasy and FluentAssertions

--- ParallelZipNet.Tests/FirstFixture.cs
+++ ParallelZipNet.Tests/FirstFixture.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using ParallelZipNet.Commands;
+﻿using ParallelZipNet.Commands;
 using Xunit;
+using FluentAssertions;
 
-namespace ParallelZipNet.Tests {
+namespace ParallelZipNet.Tests
+{
     public class FirstFixture {
         [Fact]
         public void FirstTest() {
             var section = new Section("test", new string[] { "test" }, new string[0]);
-            Assert.False(section.TryParse(new string[0], out Option option));
+            section.TryParse(new string[0], out Option option).Should().BeTrue();
         }
     }
 }
