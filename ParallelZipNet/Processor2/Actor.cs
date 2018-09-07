3680e77668692e06b0e7cc585dc20ff9d074699b
Roman Ageev <roman.ageyev@devexpress.com>
2018-09-07 10:59:23 +0300

    separate logic between bloks and channels

--- ParallelZipNet/Processor2/Actor.cs
+++ ParallelZipNet/Processor2/Actor.cs
@@ -6,6 +6,174 @@ using System.Threading.Tasks;
 using ParallelZipNet.Utils;
 
 namespace ParallelZipNet.Processor2 {
+    public interface IBlockingChannel<T> {
+        BlockingCollection<T> UnderlyingCollection { get; }
+    }
+
+    public interface IReadableChannel<T> {
+        bool Read(out T data);
+    }
+
+    public interface IWritableChannel<T> {
+        void Write(T data);
+        void Finish();
+    }
+
+    public class Channel<T> : IReadableChannel<T>, IBlockingChannel<T>, IWritableChannel<T> {
+        readonly BlockingCollection<T> collection = new BlockingCollection<T>();
+
+        BlockingCollection<T> IBlockingChannel<T>.UnderlyingCollection => collection;
+
+        public bool Read(out T data) {
+            data = default(T);
+
+            if(collection.IsCompleted)
+                return false;
+
+            try {
+                data = collection.Take();
+            }
+            catch(InvalidOperationException) {
+                return false;
+            }
+            return true;
+        }
+
+        public void Write(T data) {
+            collection.Add(data);
+        }
+
+        public void Finish() {
+            collection.CompleteAdding();
+        }
+    }
+
+    public class CompositeChannel<T> : IReadableChannel<T> {
+        readonly BlockingCollection<T>[] collections;
+
+        public CompositeChannel(IEnumerable<IBlockingChannel<T>> channels) {
+            collections = channels.Select(channel => channel.UnderlyingCollection).ToArray();
+        }
+
+        public bool Read(out T data) {
+            data = default(T);
+
+            if(collections.All(collection => collection.IsCompleted))
+                return false;
+
+            try {
+                BlockingCollection<T>.TakeFromAny(collections, out data);
+            }
+            catch(ArgumentException) {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    public delegate bool SourceAction<T>(out T data);
+
+    public class SourceChannel<T> : IReadableChannel<T> {
+        readonly SourceAction<T> action;
+
+        public SourceChannel(SourceAction<T> action) {
+            this.action = action;
+        }
+
+        public bool Read(out T data) {
+            return action(out data);
+        }
+    }
+
+    public class TargetChannel<T> : IWritableChannel<T> {
+        readonly Action<T> action;
+
+        public  TargetChannel(Action<T> action) {
+            this.action = action;
+        }
+        public void Write(T data) {
+            action(data);
+        }
+        public void Finish() {
+        }
+    }
+
+    public class Block2<T, U> {
+        readonly string name;
+        readonly Func<T, U> transform;
+        IReadableChannel<T> inputChannel;
+        IWritableChannel<U> outputChannel;
+
+        public Block2(string name, Func<T, U> transform, IReadableChannel<T> inputChannel, IWritableChannel<U> outputChannel) {
+            this.name = name;
+            this.transform = transform;
+            this.inputChannel = inputChannel;
+            this.outputChannel = outputChannel;
+        }
+
+        public Block2<U, K> Pipe<K>(Block2<U, K> block) {
+            var channel = new Channel<U>();
+
+            InitOutputChannel(channel);
+            block.InitInputChannel(channel);
+
+            return block;
+        }
+
+        public CompositeBlock2<U, K> PipeMany<K>(params Block2<U, K>[] blocks) {
+            var channel = new Channel<U>();
+
+            InitOutputChannel(channel);
+            foreach(var block in blocks)
+                block.InitInputChannel(channel);
+
+            return new CompositeBlock2<U, K>(blocks);
+        }
+
+        public Task Run() {
+            return Task.Run(() => {                
+                while(inputChannel.Read(out T data))
+                    outputChannel.Write(transform(data));
+                outputChannel.Finish();
+            });
+        }
+
+        internal void InitInputChannel(IReadableChannel<T> inputChannel) {
+            if(this.inputChannel != null)
+                throw new InvalidOperationException();
+
+            this.inputChannel = inputChannel;
+        }
+
+        internal void InitOutputChannel(IWritableChannel<U> outputChannel) {
+            if(this.outputChannel != null)
+                throw new InvalidOperationException();
+
+            this.outputChannel = outputChannel;
+        }
+    }
+
+    public class CompositeBlock2<T, U> {
+        readonly IEnumerable<Block2<T, U>> blocks;
+
+        public CompositeBlock2(IEnumerable<Block2<T, U>> blocks) {
+            this.blocks = blocks;
+        }
+
+        public Block2<U, K> Pipe<K>(Block2<U, K> block) {
+            var channels = new List<Channel<U>>();
+            foreach(var item in blocks) {
+                var channel = new Channel<U>();
+                item.InitOutputChannel(channel);
+                channels.Add(channel);
+            }
+
+            var composite = new CompositeChannel<U>(channels);
+            block.InitInputChannel(composite);
+            return block;
+        }
+    }
+
     public class Block {
         readonly string name;
         readonly Func<object, object> action;
--- ParallelZipNet/Processor2/Compressor2.cs
+++ ParallelZipNet/Processor2/Compressor2.cs
@@ -18,33 +18,61 @@ namespace ParallelZipNet.Processor2 {
             //     reader.BaseStream.CopyTo(map);
             // }
 
+            // var readEnumerator = ReadSource(reader, Constants.DEFAULT_CHUNK_SIZE).GetEnumerator();
+
+            // var blockReader = new Block("Reader", _ => {
+            //     bool next = readEnumerator.MoveNext();
+            //     if(next) {
+            //         Console.WriteLine($"{Task.CurrentId} Read Chunk\n");
+            //         return readEnumerator.Current;
+            //     }
+            //     else
+            //         return null;
+            // });
+            // var blocksZip = Enumerable.Range(0, 6).Select((_, index) => new Block($"Zip {index}", chunk => ZipChunk((Chunk)chunk))).ToArray();
+            // var blockWriter = new Block("Writer", chunk => {
+            //     Console.WriteLine($"{Task.CurrentId} Write Chunk {((Chunk)chunk).Index}");
+            //     writer.Write(((Chunk)chunk).Index);
+            //     writer.Write(((Chunk)chunk).Data.Length);
+            //     writer.Write(((Chunk)chunk).Data);
+            //     return null;
+            // });
+
+            // var pipeline = blockReader
+            //     .Pipe(blocksZip)
+            //     .Pipe(blockWriter);
+
+            // pipeline.Run()
+            //     .GetAwaiter()
+            //     .GetResult();
+
+
             var readEnumerator = ReadSource(reader, Constants.DEFAULT_CHUNK_SIZE).GetEnumerator();
 
-            var blockReader = new Block("Reader", _ => {
+            var blockReader = new Block2<Chunk, Chunk>("reader", data => data, new SourceChannel<Chunk>((out Chunk chunk) => {
                 bool next = readEnumerator.MoveNext();
-                if(next) {
-                    Console.WriteLine($"{Task.CurrentId} Read Chunk\n");
-                    return readEnumerator.Current;
-                }
-                else
-                    return null;
-            });
-            var blocksZip = Enumerable.Range(0, 6).Select((_, index) => new Block($"Zip {index}", chunk => ZipChunk((Chunk)chunk))).ToArray();
-            var blockWriter = new Block("Writer", chunk => {
-                Console.WriteLine($"{Task.CurrentId} Write Chunk {((Chunk)chunk).Index}");
-                writer.Write(((Chunk)chunk).Index);
-                writer.Write(((Chunk)chunk).Data.Length);
-                writer.Write(((Chunk)chunk).Data);
-                return null;
-            });
+                chunk = next ? readEnumerator.Current : null;
+                return next;
+            }), null);
 
-            var pipeline = blockReader
-                .Pipe(blocksZip)
-                .Pipe(blockWriter);
+            var blockZip1 = new Block2<Chunk, Chunk>("zip 1", ZipChunk, null, null);
+            var blockZip2 = new Block2<Chunk, Chunk>("zip 2", ZipChunk, null, null);
+            
+            var blockWriter = new Block2<Chunk, Chunk>("writer", data => data, null, new TargetChannel<Chunk>((Chunk chunk) => {
+                writer.Write(chunk.Index);
+                writer.Write(chunk.Data.Length);
+                writer.Write(chunk.Data);
+            }));
 
-            pipeline.Run()
-                .GetAwaiter()
-                .GetResult();
+            blockReader
+                .PipeMany<Chunk>(blockZip1, blockZip2)
+                .Pipe<Chunk>(blockWriter);
+
+            Task.WaitAll(
+                blockReader.Run(),
+                blockZip1.Run(),
+                blockZip2.Run(),
+                blockWriter.Run());
         }
 
         static IEnumerable<Chunk> ReadSource(BinaryReader reader, int chunkSize) {
