32ec83143962f1dfce00ccb72852e4a2ad3ab2f4
Roman Ageev <roman.ageyev@devexpress.com>
2018-09-26 10:05:55 +0300

    Custom channel implementation - first attempt

--- ParallelZipNet/Pipeline/Channels/Channel.cs
+++ ParallelZipNet/Pipeline/Channels/Channel.cs
@@ -1,33 +1,66 @@
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace ParallelZipNet.Pipeline.Channels {
     public class Channel<T> : IReadableChannel<T>, IWritableChannel<T> {
-        readonly BlockingCollection<T> collection = new BlockingCollection<T>(1000);
+        readonly Queue<T> queue = new Queue<T>();
+        readonly object locker = new object();
 
-        public BlockingCollection<T> UnderlyingCollection => collection;
+        bool finished = false;
+
+        // readonly BlockingCollection<T> collection = new BlockingCollection<T>(1000);
+
+        // public BlockingCollection<T> UnderlyingCollection => collection;
 
         public bool Read(out T data) {
+            return Read(out data, Timeout.Infinite).Value;
+        }
+        public bool? Read(out T data, int timeout) {
             data = default(T);
 
-            if(collection.IsCompleted)
-                return false;
+            bool result;
 
-            try {
-                data = collection.Take();                
+            lock(locker) {
+                while(queue.Count == 0 && !finished) {
+                    bool gotItem = Monitor.Wait(locker, timeout);
+                    if(!gotItem)
+                        return null;
+                }
+                
+                result = queue.Count > 0;
+                data = queue.Dequeue();                
             }
-            catch(InvalidOperationException) {
-                return false;
-            }
-            return true;
+
+            return result;
+
+            // if(collection.IsCompleted)
+            //     return false;
+
+            // try {
+            //     data = collection.Take();                
+            // }
+            // catch(InvalidOperationException) {
+            //     return false;
+            // }
+            // return true;
         }
 
         public void Write(T data) {
-            collection.Add(data);
+            lock(locker) {
+                queue.Enqueue(data);
+                Monitor.Pulse(locker);
+            }
+            // collection.Add(data);
         }
 
         public void Finish() {
-            collection.CompleteAdding();
+            lock(locker) {
+                finished = true;
+                Monitor.PulseAll(locker);
+            }
+            // collection.CompleteAdding();
         }
     }
 }
--- ParallelZipNet/Pipeline/Channels/CompositeChannel.cs
+++ ParallelZipNet/Pipeline/Channels/CompositeChannel.cs
@@ -6,7 +6,7 @@ using ParallelZipNet.Utils;
 namespace ParallelZipNet.Pipeline.Channels {
     public class CompositeChannel<T> : IReadableChannel<T> {
         readonly Channel<T>[] channels;
-        readonly BlockingCollection<T>[] collections;
+        // readonly BlockingCollection<T>[] collections;
 
         public Channel<T>[] Channels => channels;
 
@@ -14,28 +14,47 @@ namespace ParallelZipNet.Pipeline.Channels {
             Guard.NotNegative(degreeOfParallelism, nameof(degreeOfParallelism));
 
             channels = new Channel<T>[degreeOfParallelism];
-            collections = new BlockingCollection<T>[degreeOfParallelism];
+            // collections = new BlockingCollection<T>[degreeOfParallelism];
 
             for(int i = 0; i < degreeOfParallelism; i++) {
-                var channel = new Channel<T>();
-                channels[i] = channel;
-                collections[i] = channel.UnderlyingCollection;
+                channels[i] = new Channel<T>();
+                // var channel = new Channel<T>();
+                // channels[i] = channel;
+                // collections[i] = channel.UnderlyingCollection;
             }
         }
 
         public bool Read(out T data) {
             data = default(T);
 
-            if(collections.All(collection => collection.IsCompleted))
-                return false;
+            while(true) {
+                bool exit = true;
+                foreach(var channel in channels) {
+                    bool? channelResult = channel.Read(out data, 10);
+                    if(channelResult.HasValue) {
+                        if(channelResult.Value)
+                            return true;
+                    }
+                    else
+                        exit = false;
+                }
+                if(exit)
+                    return false;
+            }
 
-            try {
-                BlockingCollection<T>.TakeFromAny(collections, out data);
-            }
-            catch(ArgumentException) {
-                return false;
-            }
-            return true;
+            // bool result = false;
+
+
+            // if(collections.All(collection => collection.IsCompleted))
+            //     return false;
+
+            // try {
+            //     BlockingCollection<T>.TakeFromAny(collections, out data);
+            // }
+            // catch(ArgumentException) {
+            //     return false;
+            // }
+            // return true;
         }
     }
 }
