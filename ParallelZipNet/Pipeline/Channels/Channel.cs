93ddb01b1a3b57800a41c99d3e40c5b5e7d319a7
Roman Ageev <roman.ageyev@devexpress.com>
2018-09-27 10:06:42 +0300

    spinwait and channel.tryread

--- ParallelZipNet/Pipeline/Channels/Channel.cs
+++ ParallelZipNet/Pipeline/Channels/Channel.cs
@@ -4,64 +4,60 @@ using System.Collections.Generic;
 using System.Threading;
 
 namespace ParallelZipNet.Pipeline.Channels {
+    public enum TryReadResult { Value, NoValue, Finished }
+
     public class Channel<T> : IReadableChannel<T>, IWritableChannel<T> {
         readonly Queue<T> queue = new Queue<T>();
         readonly object locker = new object();
 
         bool finished = false;
 
-        // readonly BlockingCollection<T> collection = new BlockingCollection<T>(1000);
-
-        // public BlockingCollection<T> UnderlyingCollection => collection;
-
         public bool Read(out T data) {
-            return Read(out data, Timeout.Infinite).Value;
-        }
-        public bool? Read(out T data, int timeout) {
             data = default(T);
 
-            bool result;
-
             lock(locker) {
-                while(queue.Count == 0 && !finished) {
-                    bool gotItem = Monitor.Wait(locker, timeout);
-                    if(!gotItem)
-                        return null;
-                }
+                while(queue.Count == 0 && !finished)
+                    Monitor.Wait(locker);
                 
-                result = queue.Count > 0;
-                if(result)
-                    data = queue.Dequeue();                
+                if(queue.Count > 0) {
+                    data = queue.Dequeue();
+                    return true;
+                }
+
+                return false;                
             }
+        }
 
-            return result;
+        public TryReadResult TryRead(out T data) {
+            data = default(T);
 
-            // if(collection.IsCompleted)
-            //     return false;
+            lock(locker) {                
+                if(queue.Count > 0) {
+                    data = queue.Dequeue();
+                    return TryReadResult.Value;
+                }
 
-            // try {
-            //     data = collection.Take();                
-            // }
-            // catch(InvalidOperationException) {
-            //     return false;
-            // }
-            // return true;
+                if(finished)
+                    return TryReadResult.Finished;
+
+                return TryReadResult.NoValue;
+            }
         }
 
         public void Write(T data) {
             lock(locker) {
                 queue.Enqueue(data);
+                
                 Monitor.Pulse(locker);
             }
-            // collection.Add(data);
         }
 
         public void Finish() {
             lock(locker) {
                 finished = true;
+
                 Monitor.PulseAll(locker);
             }
-            // collection.CompleteAdding();
         }
     }
 }
--- ParallelZipNet/Pipeline/Channels/CompositeChannel.cs
+++ ParallelZipNet/Pipeline/Channels/CompositeChannel.cs
@@ -1,12 +1,12 @@
 using System;
 using System.Collections.Concurrent;
 using System.Linq;
+using System.Threading;
 using ParallelZipNet.Utils;
 
 namespace ParallelZipNet.Pipeline.Channels {
     public class CompositeChannel<T> : IReadableChannel<T> {
         readonly Channel<T>[] channels;
-        // readonly BlockingCollection<T>[] collections;
 
         public Channel<T>[] Channels => channels;
 
@@ -14,47 +14,33 @@ namespace ParallelZipNet.Pipeline.Channels {
             Guard.NotNegative(degreeOfParallelism, nameof(degreeOfParallelism));
 
             channels = new Channel<T>[degreeOfParallelism];
-            // collections = new BlockingCollection<T>[degreeOfParallelism];
-
             for(int i = 0; i < degreeOfParallelism; i++) {
                 channels[i] = new Channel<T>();
-                // var channel = new Channel<T>();
-                // channels[i] = channel;
-                // collections[i] = channel.UnderlyingCollection;
             }
         }
 
         public bool Read(out T data) {
             data = default(T);
-
+           
+            var spinWait = new SpinWait();
+            
             while(true) {
-                bool exit = true;
+                bool finish = true;
+
                 foreach(var channel in channels) {
-                    bool? channelResult = channel.Read(out data, 10);
-                    if(channelResult.HasValue) {
-                        if(channelResult.Value)
-                            return true;
-                    }
-                    else
-                        exit = false;
+                    TryReadResult channelResult = channel.TryRead(out data);
+
+                    if(channelResult == TryReadResult.Value)
+                        return true;
+                    
+                    if(channelResult == TryReadResult.NoValue)
+                        finish = false;
+
+                    spinWait.SpinOnce();
                 }
-                if(exit)
-                    return false;
+                if(finish)
+                    return false;                
             }
-
-            // bool result = false;
-
-
-            // if(collections.All(collection => collection.IsCompleted))
-            //     return false;
-
-            // try {
-            //     BlockingCollection<T>.TakeFromAny(collections, out data);
-            // }
-            // catch(ArgumentException) {
-            //     return false;
-            // }
-            // return true;
         }
     }
 }
